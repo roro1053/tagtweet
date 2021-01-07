af7bf3d794d85b5142b73be6149968b0c5fc351a
roro1053 <c1053rroro@gmail.com>
2021-01-07 12:09:28 +0900

    モデルにメソッドを定義

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,18 @@ class Post < ApplicationRecord
 
   has_many :post_tag_relations, dependent: :destroy
   has_many :tags, through: :post_tag_relations
+
+  def save_tags(tag_list)
+    tag_list.each do |tag|
+      unless find_tag = Tag.find_by(word: tag.downcase)
+        begin
+          self.tags.create!(word: tag)
+        rescue
+          nil
+        end
+      else
+        PostTagRelation.create!(post_id: self.id, tag_id: find_tag.id)
+      end
+    end
+  end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,12 @@
 class Tag < ApplicationRecord
+  before_save :downcase_tag_name
 
   has_many :post_tag_relations, dependent: :destroy
   has_many :posts, through: :post_tag_relations
+
+  private
+
+  def downcase_tag_name
+    self.tag_name.downcase!
+  end
 end
